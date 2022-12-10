6ee331cb5501da7ae40edb604eccbfec618b3018
hind333 <109863744+hind333@users.noreply.github.com>
2022-12-10 20:08:06 +0300

    تم

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 # write your code here
+person = {
+        'name': 'huda',
+        'age': '58',
+        'hobbies': ['reading', 'swimming'],
+        'country' :'syria'
+}
+print(person['name'])
+print(person['age'])
+person['age']=66
+print(person)
+print(len(person))
+person['hobbies'].append('uno player')
+print(len(person['hobbies']))
+def check_hobbies(person):
+    if len(person['hobbies']) >= 3:
+     print('WOW YOU ARE AMAZING')
+check_hobbies(person)
